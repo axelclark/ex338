df95c06518157ed91245e70e27fffb34932b50f0
Axel Clark <axelclark2@yahoo.com>
2016-07-09 15:52:18 -0400

    Add credo

--- lib/ex338.ex
+++ lib/ex338.ex
@@ -1,4 +1,6 @@
 defmodule Ex338 do
+  @moduledoc false
+
   use Application
 
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
--- mix.exs
+++ mix.exs
@@ -30,14 +30,16 @@ defmodule Ex338.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.0"},
-     {:phoenix_pubsub, "~> 1.0"},
+    [{:credo, "~> 0.4", only: [:dev, :test]},
+     {:cowboy, "~> 1.0"},
+     {:gettext, "~> 0.11"},
+     {:phoenix, "~> 1.2.0"},
      {:phoenix_ecto, "~> 3.0"},
-     {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:phoenix_pubsub, "~> 1.0"},
+     {:postgrex, ">= 0.0.0"},
+   ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.

22086e42eb6b744b1aff661950fe21fc09b06c90
Axel Clark <axelclark2@yahoo.com>
2016-07-13 21:32:30 -0400

    Add preload and Fantasy League to template

--- web/controllers/fantasy_team_controller.ex
+++ web/controllers/fantasy_team_controller.ex
@@ -1,13 +1,17 @@
 defmodule Ex338.FantasyTeamController do
   use Ex338.Web, :controller
 
-  alias Ex338.FantasyTeam
+  alias Ex338.{FantasyTeam, FantasyLeague}
 
   def index(conn, %{"fantasy_league_id" => league_id}) do
+    fantasy_league = FantasyLeague |> Repo.get(league_id)
+
     fantasy_teams = FantasyTeam
                     |> FantasyTeam.by_league(league_id)
+                    |> preload(roster_positions: [fantasy_player: :sports_league])
                     |> Repo.all
 
-    render(conn, "index.html", fantasy_teams: fantasy_teams)
+    render(conn, "index.html", fantasy_league: fantasy_league, 
+                               fantasy_teams: fantasy_teams)
   end
 end

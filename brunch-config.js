c38df3e2737dd4c36d90fe0d30c058cc8ec32b63
Axel Clark <axelclark2@yahoo.com>
2016-07-14 20:47:42 -0400

    Add bourbon, neat, and transfer css
    Installed bourbon and neat.  Removed bootstrap.
    Transferred scss files from Rails 338 app.

--- brunch-config.js
+++ brunch-config.js
@@ -1,3 +1,6 @@
+var neat = require("bourbon-neat").includePaths
+var bourbon = require("bourbon").includePaths
+
 exports.config = {
   // See http://brunch.io/#documentation for docs.
   files: {
@@ -54,6 +57,11 @@ exports.config = {
     babel: {
       // Do not use ES6 compiler in vendor code
       ignore: [/web\/static\/vendor/]
+    },
+    sass: {
+      options: {
+        includePaths: bourbon.concat(neat) 
+      }
     }
   },
 

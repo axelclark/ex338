e6906a3f8dd4972506eafe66c0e9aece0b0a6a4c
Axel Clark <axelclark2@yahoo.com>
2016-07-23 08:28:42 -0700

    Add draft pick seeds

--- priv/repo/dev_seeds.exs
+++ priv/repo/dev_seeds.exs
@@ -1,4 +1,19 @@
-alias Ex338.{RosterPosition, RosterTransaction, TransactionLineItem, Repo}
+# Script for populating the database. You can run it as:
+#
+#     mix run priv/repo/dev_seeds.exs
+#
+# Inside the script, you can read and write to any of your
+# repositories directly:
+#
+#     Ex338.Repo.insert!(%Ex338.SomeModel{})
+#
+# We recommend using the bang functions (`insert!`, `update!`
+# and so on) as they will fail if something goes wrong.
+# 
+# To substitue hidden characters in VIM %s/<ctr>v<ctrl>m/\r/g
+
+alias Ex338.{RosterPosition, RosterTransaction, TransactionLineItem, Repo, 
+             DraftPick}
 
 defmodule Ex338.DevSeeds do
   def store_roster_positions(row) do
@@ -13,6 +28,11 @@ defmodule Ex338.DevSeeds do
   
   def store_transaction_line_items(row) do
     changeset = TransactionLineItem.changeset(%TransactionLineItem{}, row)
+    Repo.insert!(changeset)
+  end
+  
+  def store_draft_picks(row) do
+    changeset = DraftPick.changeset(%DraftPick{}, row)
     Repo.insert!(changeset)
   end
 end
@@ -30,3 +50,8 @@ File.stream!("priv/repo/csv_seed_data/transaction_line_items.csv")
   |> CSV.decode(headers: [:fantasy_team_id, :fantasy_player_id, :action, 
                           :roster_transaction_id])
   |> Enum.each(&Ex338.DevSeeds.store_transaction_line_items/1)
+
+File.stream!("priv/repo/csv_seed_data/draft_picks.csv")
+  |> Stream.drop(1)
+  |> CSV.decode(headers: [:draft_position, :fantasy_league_id, :fantasy_team_id])
+  |> Enum.each(&Ex338.DevSeeds.store_draft_picks/1)

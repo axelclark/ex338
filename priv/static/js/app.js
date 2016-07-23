22d83038b42d242caae4967516628ecf0ef4a0ed
Axel Clark <axelclark2@yahoo.com>
2016-07-23 14:47:27 -0700

    Update header navigation using a refill

--- priv/static/js/app.js
+++ priv/static/js/app.js
@@ -1458,9 +1458,48 @@ require.register("web/static/js/app.js", function(exports, require, module) {
 "use strict";
 
 require("phoenix_html");
+
+require("./header");
 });
 
-;require.register("web/static/js/socket.js", function(exports, require, module) {
+;require.register("web/static/js/header.js", function(exports, require, module) {
+"use strict";
+
+$(window).resize(function () {
+  var more = document.getElementById("js-navigation-more");
+  if ($(more).length > 0) {
+    var windowWidth = $(window).width();
+    var moreLeftSideToPageLeftSide = $(more).offset().left;
+    var moreLeftSideToPageRightSide = windowWidth - moreLeftSideToPageLeftSide;
+
+    if (moreLeftSideToPageRightSide < 330) {
+      $("#js-navigation-more .submenu .submenu").removeClass("fly-out-right");
+      $("#js-navigation-more .submenu .submenu").addClass("fly-out-left");
+    }
+
+    if (moreLeftSideToPageRightSide > 330) {
+      $("#js-navigation-more .submenu .submenu").removeClass("fly-out-left");
+      $("#js-navigation-more .submenu .submenu").addClass("fly-out-right");
+    }
+  }
+});
+
+$(document).ready(function () {
+  var menuToggle = $("#js-mobile-menu").unbind();
+  $("#js-navigation-menu").removeClass("show");
+
+  menuToggle.on("click", function (e) {
+    e.preventDefault();
+    $("#js-navigation-menu").slideToggle(function () {
+      if ($("#js-navigation-menu").is(":hidden")) {
+        $("#js-navigation-menu").removeAttr("style");
+      }
+    });
+  });
+});
+});
+
+require.register("web/static/js/socket.js", function(exports, require, module) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
--- web/static/js/app.js
+++ web/static/js/app.js
@@ -13,9 +13,12 @@
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
 
+
 // Import local files
 //
 // Local files can be imported directly using relative
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+import "./header"

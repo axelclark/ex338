3b65b22a10b54143f2223fa6a70ecf3ccc493919
Axel Clark <axelclark2@yahoo.com>
2016-07-24 14:50:15 -0700

    Add select for position on Roster Position admin
    * Add position model attribute to enable drop down
    * Later implementation may need another model or query Sports League

--- web/admin/roster_position.ex
+++ web/admin/roster_position.ex
@@ -5,5 +5,16 @@ defmodule Ex338.ExAdmin.RosterPosition do
 
   register_resource Ex338.RosterPosition do
 
+    form roster_position do
+      inputs do
+        input roster_position, :position,
+                               collection: Ex338.RosterPosition.positions
+        input roster_position, :fantasy_team,
+                               collection: Ex338.FantasyTeam.all
+        input roster_position, :fantasy_player,
+                               collection: Ex338.FantasyPlayer.all
+      end
+    end
+
   end
 end
--- web/models/roster_position.ex
+++ web/models/roster_position.ex
@@ -3,6 +3,9 @@ defmodule Ex338.RosterPosition do
 
   use Ex338.Web, :model
 
+  @positions ["CL", "CBB", "CFB", "CH", "KD", "LLWS", "MLB", "NBA", "NFL", 
+              "NHL", "PGA", "SC", "Tennis(M)", "Tennis(W)", "Any"]
+
   schema "roster_positions" do
     field :position, :string
     belongs_to :fantasy_team, Ex338.FantasyTeam
@@ -19,4 +22,6 @@ defmodule Ex338.RosterPosition do
     |> cast(params, [:position, :fantasy_team_id, :fantasy_player_id])
     |> validate_required([:position, :fantasy_team_id])
   end
+
+  def positions, do: @positions
 end

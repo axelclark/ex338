ceee05c1395e55cc7e8d3c9693bc92076d94629c
Axel Clark <axelclark2@yahoo.com>
2016-07-29 18:39:39 -0400

    Updates unused test varable and admin formatting
    * Add underscore to unused test variable
    * Change scrivener default to 20
    * Reorder additional terms last for admin show page

--- test/models/roster_transaction_repo_test.exs
+++ test/models/roster_transaction_repo_test.exs
@@ -7,22 +7,22 @@ defmodule Ex338.RosterTransactionRepoTest do
       league = insert(:fantasy_league)
       other_league = insert(:fantasy_league)
       team = insert(:fantasy_team, team_name: "a", fantasy_league: league)
-      other_team = insert(:fantasy_team, team_name: "b", 
+      _other_team = insert(:fantasy_team, team_name: "b",
                                          fantasy_league: other_league)
       player = insert(:fantasy_player)
       other_player = insert(:fantasy_player)
       roster_transaction = insert(:roster_transaction)
-      insert(:transaction_line_item, fantasy_team: team, 
+      insert(:transaction_line_item, fantasy_team: team,
                                      fantasy_player: player,
                                      roster_transaction: roster_transaction)
-      insert(:transaction_line_item, fantasy_team: team, 
+      insert(:transaction_line_item, fantasy_team: team,
                                      fantasy_player: other_player,
                                      roster_transaction: roster_transaction)
 
       query = RosterTransaction |> RosterTransaction.by_league(league.id)
       query = from r in query, select: r.roster_transaction_on
 
-      assert Repo.all(query) == [roster_transaction.roster_transaction_on] 
+      assert Repo.all(query) == [roster_transaction.roster_transaction_on]
     end
   end
 end

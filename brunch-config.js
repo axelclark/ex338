3f376f224772eea7cf64ff4b2143c518279fc619
Axel Clark <axelclark2@yahoo.com>
2016-09-17 18:25:56 -0400

    Add exadmin css/js to web/static and delete from priv/static
    * Update with latest ExAdmin release
    * Updated .gitignore since priv/static no longer required

--- brunch-config.js
+++ brunch-config.js
@@ -5,7 +5,13 @@ exports.config = {
   // See http://brunch.io/#documentation for docs.
   files: {
     javascripts: {
-      joinTo: "js/app.js"
+      joinTo: {
+        "js/app.js": /^(web\/static\/js)|(node_modules)/,
+        "js/ex_admin_common.js": ["web/static/vendor/ex_admin_common.js"],
+        "js/admin_lte2.js": ["web/static/vendor/admin_lte2.js"],
+        "js/jquery.min.js": ["web/static/vendor/jquery.min.js"],
+      }
+    },
 
       // To use a separate vendor.js bundle, specify two files path
       // http://brunch.io/docs/config#-files-
@@ -21,9 +27,12 @@ exports.config = {
       //     "web/static/vendor/js/bootstrap.min.js"
       //   ]
       // }
-    },
     stylesheets: {
-      joinTo: "css/app.css",
+      joinTo: {
+        "css/app.css": /^(web\/static\/css)/,
+        "css/admin_lte2.css": ["web/static/vendor/admin_lte2.css"],
+        "css/active_admin.css": ["web/static/vendor/active_admin.css.css"],
+      },
       order: {
         after: ["web/static/css/app.css"] // concat app.css last
       }
@@ -75,3 +84,45 @@ exports.config = {
     enabled: true
   }
 };
+
+// To add the ExAdmin generated assets to your brunch build, do the following:
+//
+// Replace
+//
+//     javascripts: {
+//       joinTo: "js/app.js"
+//     }
+//
+// With
+//
+//     javascripts: {
+//       joinTo: {
+//         "js/app.js": /^(web\/static\/js)|(node_modules)/,
+//         "js/ex_admin_common.js": ["web/static/vendor/ex_admin_common.js"],
+//         "js/admin_lte2.js": ["web/static/vendor/admin_lte2.js"],
+//         "js/jquery.min.js": ["web/static/vendor/jquery.min.js"],
+//       }
+//     },
+//
+// Replace
+//
+//     stylesheets: {
+//       joinTo: "css/app.css",
+//       order: {
+//         after: ["web/static/css/app.css"] // concat app.css last
+//       }
+//     },
+//
+// With
+//
+//     stylesheets: {
+//       joinTo: {
+//         "css/app.css": /^(web\/static\/css)/,
+//         "css/admin_lte2.css": ["web/static/vendor/admin_lte2.css"],
+//         "css/active_admin.css": ["web/static/vendor/active_admin.css.css"],
+//       },
+//       order: {
+//         after: ["web/static/css/app.css"] // concat app.css last
+//       }
+//     },
+//

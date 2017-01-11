cc194b1604cc59b149b8ab6fc62b4cb4fb828e04
Axel Clark <axelclark2@yahoo.com>
2017-01-10 20:48:19 -0500

    Add ranking to standings page
    * The standings page displays the rank of the team instead of "-"
    * Does not handle ties yet and second sort is by waiver position
    * Closes #165

--- web/views/fantasy_league_view.ex
+++ web/views/fantasy_league_view.ex
@@ -3,7 +3,21 @@ defmodule Ex338.FantasyLeagueView do
 
   import Ex338.FantasyTeamView, only: [calculate_points: 1]
 
+  def sort_and_rank(fantasy_teams) do
+    fantasy_teams
+    |> sort_by_points
+    |> add_rank
+  end
+
   def sort_by_points(fantasy_teams) do
     Enum.sort(fantasy_teams, &(calculate_points(&1) >= calculate_points(&2)))
+  end
+
+  defp add_rank(fantasy_teams) do
+    {teams, _} = Enum.map_reduce fantasy_teams, 1, fn(team, acc) ->
+     {Map.put(team, :rank, acc), acc + 1}
+    end
+
+    teams
   end
 end

ae7d6ff535d2c335d7ad874fb17623982cf81d28
Axel Clark <axelclark2@yahoo.com>
2017-10-14 11:05:06 -0400

    Update Phoenix

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ config :ex338, Ex338.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "the338challenge.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json",
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production

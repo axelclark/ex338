d5f303d11ca930e5c81d18c89762bb3554d2250a
Axel Clark <axelclark2@yahoo.com>
2018-11-02 00:44:26 -0400

    Automate location of rules in header
    * Automate location of rules in header based on Div A
    * Puts rules directly above the Div A for each year
    * Fixes typo in FantasyLeague seed data
    * Closes #484

--- lib/ex338_web/views/layout_view.ex
+++ lib/ex338_web/views/layout_view.ex
@@ -6,7 +6,6 @@ defmodule Ex338Web.LayoutView do
     |> filter_leagues(navbar)
     |> sort_by_div
     |> sort_by_year
-    |> display_name
   end
 
   ## Helpers
@@ -17,17 +16,11 @@ defmodule Ex338Web.LayoutView do
     Enum.filter(fantasy_leagues, &(&1.navbar_display == navbar))
   end
 
-  defp sort_by_year(fantasy_leagues) do
-    Enum.sort_by(fantasy_leagues, & &1.year)
-  end
-
   defp sort_by_div(fantasy_leagues) do
     Enum.sort_by(fantasy_leagues, & &1.division)
   end
 
-  defp display_name(fantasy_leagues) do
-    Enum.map(fantasy_leagues, fn league ->
-      %{id: league.id, name: "#{league.year} Div #{league.division}"}
-    end)
+  defp sort_by_year(fantasy_leagues) do
+    Enum.sort_by(fantasy_leagues, & &1.year)
   end
 end
--- test/ex338_web/views/layout_view_test.exs
+++ test/ex338_web/views/layout_view_test.exs
@@ -5,30 +5,49 @@ defmodule Ex338Web.LayoutViewTest do
   alias Ex338.{FantasyLeague}
 
   @leagues [
-    %FantasyLeague{id: 5, navbar_display: :hidden, division: "NA", year: 2019},
-    %FantasyLeague{id: 4, navbar_display: :primary, division: "A", year: 2019},
-    %FantasyLeague{id: 3, navbar_display: :primary, division: "A", year: 2018},
-    %FantasyLeague{id: 2, navbar_display: :primary, division: "B", year: 2018},
-    %FantasyLeague{id: 1, navbar_display: :archived, division: "A", year: 2017}
+    %FantasyLeague{
+      id: 5,
+      navbar_display: :hidden,
+      division: "NA",
+      year: 2019
+    },
+    %FantasyLeague{
+      id: 4,
+      navbar_display: :primary,
+      division: "A",
+      year: 2019
+    },
+    %FantasyLeague{
+      id: 3,
+      navbar_display: :primary,
+      division: "A",
+      year: 2018
+    },
+    %FantasyLeague{
+      id: 2,
+      navbar_display: :primary,
+      division: "B",
+      year: 2018
+    },
+    %FantasyLeague{
+      id: 1,
+      navbar_display: :archived,
+      division: "A",
+      year: 2017
+    }
   ]
 
   describe "display/2" do
     test "returns primary leagues for navbar display" do
       result = LayoutView.display(@leagues, :primary)
 
-      assert result == [
-               %{id: 3, name: "2018 Div A"},
-               %{id: 2, name: "2018 Div B"},
-               %{id: 4, name: "2019 Div A"}
-             ]
+      assert Enum.map(result, & &1.id) == [3, 2, 4]
     end
 
     test "returns archived leagues for navbar display" do
-      result = LayoutView.display(@leagues, :archived)
+      [result] = LayoutView.display(@leagues, :archived)
 
-      assert result == [
-               %{id: 1, name: "2017 Div A"}
-             ]
+      assert result.id == 1
     end
   end
 end

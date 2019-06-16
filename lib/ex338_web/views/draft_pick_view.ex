c824cf252ba8999479c321a05434fdd6fc3346ff
Axel Clark <axelclark2@yahoo.com>
2019-06-15 21:03:23 -0400

    Add Current Picks to top of DraftPick index
    * Add DraftPickView.current_picks/2 to get recent & up next picks
    * LiveView will update in real-time
    * #593

--- lib/ex338_web/views/draft_pick_view.ex
+++ lib/ex338_web/views/draft_pick_view.ex
@@ -1,6 +1,28 @@
 defmodule Ex338Web.DraftPickView do
   use Ex338Web, :view
 
+  def current_picks(draft_picks, amount) when amount >= 0 do
+    next_pick_index = Enum.find_index(draft_picks, &(&1.fantasy_player_id == nil))
+    get_current_picks(draft_picks, next_pick_index, amount)
+  end
+
+  defp get_current_picks(draft_picks, nil, amount) do
+    Enum.take(draft_picks, -div(amount, 2))
+  end
+
+  defp get_current_picks(draft_picks, index, amount) do
+    start_index = index - div(amount, 2)
+
+    start_index =
+      if start_index < 0 do
+        0
+      else
+        start_index
+      end
+
+    Enum.slice(draft_picks, start_index, amount)
+  end
+
   def next_pick?(draft_picks, draft_pick) do
     Enum.find(draft_picks, &(&1.fantasy_player_id == nil)) == draft_pick
   end
--- test/ex338_web/views/draft_pick_view_test.exs
+++ test/ex338_web/views/draft_pick_view_test.exs
@@ -2,6 +2,38 @@ defmodule Ex338Web.DraftPickViewTest do
   use Ex338Web.ConnCase, async: true
   alias Ex338Web.{DraftPickView}
 
+  describe "current_picks/2" do
+    test "returns last picks if no picks remaining" do
+      amount = 10
+      draft_picks = for n <- 1..16, do: %{draft_position: n, fantasy_player_id: n}
+
+      results = DraftPickView.current_picks(draft_picks, amount)
+
+      assert Enum.map(results, & &1.draft_position) == Enum.to_list(12..16)
+    end
+
+    test "returns first picks if no picks completed" do
+      amount = 5
+      draft_picks = for n <- 1..16, do: %{draft_position: n, fantasy_player_id: nil}
+
+      results = DraftPickView.current_picks(draft_picks, amount)
+
+      assert Enum.map(results, & &1.draft_position) == Enum.to_list(1..5)
+    end
+
+    test "returns last 5 and next 5 picks when amount 10 is provided" do
+      amount = 10
+      completed_draft_picks = for n <- 1..8, do: %{draft_position: n, fantasy_player_id: n}
+      remaining_draft_picks = for n <- 9..16, do: %{draft_position: n, fantasy_player_id: nil}
+
+      draft_picks = completed_draft_picks ++ remaining_draft_picks
+
+      results = DraftPickView.current_picks(draft_picks, amount)
+
+      assert Enum.map(results, & &1.draft_position) == Enum.to_list(4..13)
+    end
+  end
+
   describe "next_pick?/2" do
     test "returns true if the pick is next" do
       completed_pick = %{draft_position: 1, fantasy_player_id: 1}

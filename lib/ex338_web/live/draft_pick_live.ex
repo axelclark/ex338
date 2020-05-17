e5cb32ce554a95f12e50577a812f0aafdd3f1f77
Axel Clark <axel.clark@pm.me>
2020-05-17 14:38:07 -0400

    Add sport filtering to draft picks index
    * Add sport filtering to draft picks index with LiveView
    * Need to maintain draft picks & filtered picks for current picks
    * Closes #696

--- lib/ex338_web/live/draft_pick_live.ex
+++ lib/ex338_web/live/draft_pick_live.ex
@@ -12,12 +12,19 @@ defmodule Ex338Web.DraftPickLive do
     %{draft_picks: picks, fantasy_teams: teams} =
       DraftPick.Store.get_picks_for_league(fantasy_league_id)
 
+    sports_league_id = ""
+
+    filtered_draft_picks = filter_draft_picks_by_sport(picks, sports_league_id)
+
     socket =
       socket
-      |> assign(:draft_picks, picks)
       |> assign(:fantasy_teams, teams)
+      |> assign(:draft_picks, picks)
+      |> assign(:sports_league_id, sports_league_id)
+      |> assign(:filtered_draft_picks, filtered_draft_picks)
       |> assign_new(:current_user, fn -> User.Store.get_user!(current_user_id) end)
       |> assign_new(:fantasy_league, fn -> FantasyLeague.Store.get(fantasy_league_id) end)
+      |> assign_new(:sports_league_options, fn -> sports_league_options(picks) end)
 
     # need to clear flash from controller so not displayed twice
     socket = clear_flash(socket)
@@ -32,8 +39,53 @@ defmodule Ex338Web.DraftPickLive do
   def handle_info({"draft_pick", [:draft_pick | _], _}, socket) do
     new_data = DraftPick.Store.get_picks_for_league(socket.assigns.fantasy_league.id)
 
-    socket = put_flash(socket, :info, "New pick!")
+    filtered_draft_picks =
+      filter_draft_picks_by_sport(new_data.draft_picks, socket.assigns.sports_league_id)
+
+    socket =
+      socket
+      |> assign(new_data)
+      |> assign(filtered_draft_picks: filtered_draft_picks)
+      |> put_flash(:info, "New pick!")
 
     {:noreply, assign(socket, new_data)}
+  end
+
+  def handle_event("filter", %{"sports_league_id" => sport_id}, socket) do
+    picks = filter_draft_picks_by_sport(socket.assigns.draft_picks, sport_id)
+    socket = assign(socket, sports_league_id: sport_id, filtered_draft_picks: picks)
+    {:noreply, socket}
+  end
+
+  ## Helpers
+
+  defp filter_draft_picks_by_sport(draft_picks, ""), do: draft_picks
+
+  defp filter_draft_picks_by_sport(draft_picks, sports_league_id) do
+    sports_league_id = String.to_integer(sports_league_id)
+
+    Enum.filter(draft_picks, fn
+      %{fantasy_player: %{sports_league: sports_league}} ->
+        sports_league.id == sports_league_id
+
+      _ ->
+        false
+    end)
+  end
+
+  defp sports_league_options(draft_picks) do
+    options =
+      draft_picks
+      |> Enum.reduce([], fn
+        %{fantasy_player: %{sports_league: sports_league}}, options ->
+          [{sports_league.abbrev, sports_league.id}] ++ options
+
+        _, options ->
+          options
+      end)
+      |> Enum.uniq()
+      |> Enum.sort_by(fn {abbrev, _} -> abbrev end)
+
+    [{"All Sports", ""}] ++ options
   end
 end
--- test/ex338_web/controllers/draft_pick_controller_test.exs
+++ test/ex338_web/controllers/draft_pick_controller_test.exs
@@ -47,6 +47,61 @@ defmodule Ex338Web.DraftPickControllerTest do
 
       assert render(view) =~ player.player_name
     end
+
+    test "filters draft picks by sport without changing current picks", %{
+      conn: conn,
+      user: user
+    } do
+      insert(:user, id: user.id)
+      league = insert(:fantasy_league)
+      team = insert(:fantasy_team, team_name: "Brown", fantasy_league: league)
+
+      sport = insert(:sports_league)
+      player = insert(:fantasy_player, sports_league: sport)
+      insert(:league_sport, sports_league: sport, fantasy_league: league)
+
+      insert(:draft_pick,
+        draft_position: 1.01,
+        fantasy_team: team,
+        fantasy_league: league,
+        fantasy_player: player
+      )
+
+      other_sport = insert(:sports_league)
+      other_player = insert(:fantasy_player, sports_league: other_sport)
+      insert(:league_sport, sports_league: other_sport, fantasy_league: league)
+
+      insert(
+        :draft_pick,
+        draft_position: 1.02,
+        fantasy_team: team,
+        fantasy_league: league,
+        fantasy_player: other_player
+      )
+
+      extra_sport = insert(:sports_league)
+      insert(:fantasy_player, sports_league: extra_sport)
+      insert(:league_sport, sports_league: extra_sport, fantasy_league: league)
+
+      insert_list(5, :submitted_pick,
+        draft_position: 1.03,
+        fantasy_league: league
+      )
+
+      insert_list(5, :draft_pick, draft_position: 1.04, fantasy_league: league)
+
+      conn = assign(conn, :live_module, Ex338Web.DraftPickLive)
+
+      {:ok, view, html} = live(conn, fantasy_league_draft_pick_path(conn, :index, league.id))
+
+      assert String.contains?(html, player.player_name)
+      assert String.contains?(html, other_player.player_name)
+
+      live_view = render_change(view, :filter, %{sports_league_id: sport.id})
+
+      assert live_view =~ player.player_name
+      refute live_view =~ other_player.player_name
+    end
   end
 
   describe "edit/2" do

ddbd4e38ebfe296b851bfa6e2230fe712753c9f8
Axel Clark <axel.clark@pm.me>
2020-06-13 21:45:46 -0400

    Update context.sh

--- context.sh
+++ context.sh
@@ -3,10 +3,21 @@
 RESOURCE="$1"
 CONTEXT="$2"
 
-mv "lib/ex338/"$RESOURCE"/"* "lib/ex338/"$CONTEXT"/"
-rm -d "lib/ex338/"$RESOURCE"/"
+mv "lib/ex338/"$RESOURCE"/store.ex" "lib/ex338/"$CONTEXT".ex"
+mv "lib/ex338/"$RESOURCE"" "lib/ex338/"$CONTEXT""
 mv "lib/ex338/"$RESOURCE".ex" "lib/ex338/"$CONTEXT"/"$RESOURCE".ex"
 
-mv "test/ex338/"$RESOURCE"/"* "test/ex338/"$CONTEXT"/"
-rm -d "test/ex338/"$RESOURCE"/"
+mv "test/ex338/"$RESOURCE"/store_test.exs" "test/ex338/"$CONTEXT"_test.exs"
+mv "test/ex338/"$RESOURCE"" "test/ex338/"$CONTEXT""
 mv "test/ex338/"$RESOURCE"_test.exs" "test/ex338/"$CONTEXT"/"$RESOURCE"_test.exs"
+
+# RESOURCE="$1"
+# CONTEXT="$2"
+
+# mv "lib/ex338/"$RESOURCE"/"* "lib/ex338/"$CONTEXT"/"
+# rm -d "lib/ex338/"$RESOURCE"/"
+# mv "lib/ex338/"$RESOURCE".ex" "lib/ex338/"$CONTEXT"/"$RESOURCE".ex"
+
+# mv "test/ex338/"$RESOURCE"/"* "test/ex338/"$CONTEXT"/"
+# rm -d "test/ex338/"$RESOURCE"/"
+# mv "test/ex338/"$RESOURCE"_test.exs" "test/ex338/"$CONTEXT"/"$RESOURCE"_test.exs"

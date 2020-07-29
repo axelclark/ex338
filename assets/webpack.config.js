e77ba6a9f2b06fad24e16d961afb18a9dd985ac2
Axel Clark <axel.clark@pm.me>
2020-07-28 20:43:49 -0700

    Update NPM deps
    * Update tailwindcss packages
    * Update webpack-copy-plugin and fix breaking change

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,27 +1,27 @@
-const path = require("path");
-const glob = require("glob");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path")
+const glob = require("glob")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const TerserPlugin = require("terser-webpack-plugin")
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin")
+const CopyPlugin = require("copy-webpack-plugin")
 
 module.exports = (env, options) => {
-  const devMode = options.mode !== "production";
+  const devMode = options.mode !== "production"
 
   return {
     optimization: {
       minimizer: [
         new TerserPlugin({ cache: true, parallel: true, sourceMap: devMode }),
-        new OptimizeCSSAssetsPlugin({})
-      ]
+        new OptimizeCSSAssetsPlugin({}),
+      ],
     },
     entry: {
-      app: glob.sync("./vendor/**/*.js").concat(["./js/app.js"])
+      app: glob.sync("./vendor/**/*.js").concat(["./js/app.js"]),
     },
     output: {
       filename: "[name].js",
       path: path.resolve(__dirname, "../priv/static/js"),
-      publicPath: "/js/"
+      publicPath: "/js/",
     },
     devtool: devMode ? "source-map" : undefined,
     module: {
@@ -30,22 +30,22 @@ module.exports = (env, options) => {
           test: /\.js$/,
           exclude: /node_modules/,
           use: {
-            loader: "babel-loader"
-          }
+            loader: "babel-loader",
+          },
         },
         {
           test: /\.css$/,
           use: [
             MiniCssExtractPlugin.loader,
             "css-loader?url=false",
-            "postcss-loader"
-          ]
-        }
-      ]
+            "postcss-loader",
+          ],
+        },
+      ],
     },
     plugins: [
       new MiniCssExtractPlugin({ filename: "../css/app.css" }),
-      new CopyWebpackPlugin([{ from: "static/", to: "../" }])
-    ]
-  };
-};
+      new CopyPlugin({ patterns: [{ from: "static/", to: "../" }] }),
+    ],
+  }
+}

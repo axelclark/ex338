e0829dbcbbbc349db2bffe76611b6acb1b0af37b
Axel Clark <axel.clark@pm.me>
2020-08-19 10:13:40 -0700

    Fix bug in filter_players.js
    * Fix bug in filter_players.js
    * Using hidden attribute doesn't seem to work on mobile
    * Closes #920

--- assets/js/filter_players.js
+++ assets/js/filter_players.js
@@ -3,22 +3,31 @@ const sportSelect = document.querySelector(".sports-select-filter")
 if (sportSelect) {
   sportSelect.onchange = filterPlayers
 }
+const playerOptions = document.querySelectorAll(".players-to-filter option")
+
+const players = Array.from(playerOptions)
 
 function filterPlayers(event) {
   const sportAbbrev = event.target.value
 
-  const players = Array.from(
-    document.querySelectorAll(".players-to-filter option")
-  )
+  const playersSelect = document.querySelector(".players-to-filter")
 
-  players.forEach((player) => {
-    player.removeAttribute("hidden")
+  removePlayerOptions(playersSelect)
 
-    if (player.className !== sportAbbrev) {
-      player.setAttribute("hidden", "true")
-    }
+  const filteredPlayers = players.filter((player) => {
+    return player.className === sportAbbrev
   })
 
-  players[0].selected = true
-  players[0].removeAttribute("hidden")
+  filteredPlayers.forEach(function (player) {
+    const newPlayer = player.cloneNode(true)
+    playersSelect.appendChild(newPlayer)
+  })
+
+  playersSelect.options[0].selected = true
+}
+
+function removePlayerOptions(selectElement) {
+  while (selectElement.firstChild) {
+    selectElement.removeChild(selectElement.firstChild)
+  }
 }

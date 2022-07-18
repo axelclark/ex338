7e2869392f1ba50f1bbf4e07d57f2a0e4d92b11a
Axel Clark <axel.clark@pm.me>
2022-07-17 21:43:51 -0700

    Fix warning for live component
    * Remove socket as it is no longer required
    * See #1071

--- lib/ex338_web/live/live_helpers.ex
+++ lib/ex338_web/live/live_helpers.ex
@@ -15,9 +15,9 @@ defmodule Ex338Web.LiveHelpers do
         post: @post,
         return_to: Routes.post_index_path(@socket, :index) %>
   """
-  def live_modal(socket, component, opts) do
+  def live_modal(_socket, component, opts) do
     path = Keyword.fetch!(opts, :return_to)
     modal_opts = [id: :modal, return_to: path, component: component, opts: opts]
-    live_component(socket, Ex338Web.ModalComponent, modal_opts)
+    live_component(Ex338Web.ModalComponent, modal_opts)
   end
 end

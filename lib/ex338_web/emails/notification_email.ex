6f419cc60e240f9737736646b899accccd841584
Axel Clark <axel.clark@pm.me>
2022-07-23 09:10:48 -0700

    Update 1st line of draft email
    * Update 1st line of draft email so pick is in email body preview
    * Closes #1088

--- lib/ex338_web/emails/notification_email.ex
+++ lib/ex338_web/emails/notification_email.ex
@@ -22,15 +22,14 @@ defmodule Ex338Web.NotificationEmail do
     |> subject(draft_headline(draft_pick, league))
     |> render_body("draft_update.html", %{
       league: league,
+      draft_pick: draft_pick,
       last_picks: last_picks,
       next_picks: next_picks
     })
   end
 
   defp draft_headline(draft_pick, league) do
-    "338 Draft - #{league.fantasy_league_name}: #{draft_pick.fantasy_team.team_name} selects #{
-      draft_pick.fantasy_player.player_name
-    } (##{draft_pick.draft_position})"
+    "338 Draft - #{league.fantasy_league_name}: #{draft_pick.fantasy_team.team_name} selects #{draft_pick.fantasy_player.player_name} (##{draft_pick.draft_position})"
   end
 
   def in_season_draft_update(
@@ -48,9 +47,7 @@ defmodule Ex338Web.NotificationEmail do
     last_pick = Enum.at(last_picks, 0)
     fantasy_team = last_pick.draft_pick_asset.fantasy_team
 
-    "338 Draft - #{fantasy_league.fantasy_league_name}: #{fantasy_team.team_name} selects #{
-      last_pick.drafted_player.player_name
-    } (##{last_pick.position})"
+    "338 Draft - #{fantasy_league.fantasy_league_name}: #{fantasy_team.team_name} selects #{last_pick.drafted_player.player_name} (##{last_pick.position})"
   end
 
   def waiver_submitted(%Waiver{id: waiver_id}) do
@@ -71,14 +68,10 @@ defmodule Ex338Web.NotificationEmail do
          %Waiver{add_fantasy_player_id: nil, drop_fantasy_player: player} = waiver,
          fantasy_league
        ) do
-    "338 Waiver - #{fantasy_league.fantasy_league_name}: #{waiver.fantasy_team.team_name} drops #{
-      player.player_name
-    } (#{player.sports_league.abbrev})"
+    "338 Waiver - #{fantasy_league.fantasy_league_name}: #{waiver.fantasy_team.team_name} drops #{player.player_name} (#{player.sports_league.abbrev})"
   end
 
   defp waiver_headline(%Waiver{add_fantasy_player: player} = waiver, fantasy_league) do
-    "338 Waiver - #{fantasy_league.fantasy_league_name}: #{waiver.fantasy_team.team_name} claims #{
-      display_name(player)
-    } (#{player.sports_league.abbrev})"
+    "338 Waiver - #{fantasy_league.fantasy_league_name}: #{waiver.fantasy_team.team_name} claims #{display_name(player)} (#{player.sports_league.abbrev})"
   end
 end

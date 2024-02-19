84e292362cc83c3eb509a5ed0ecb9759766f4f56
Axel Clark <axel.clark@pm.me>
2024-02-19 15:17:11 -0800

    Convert fantasy_league_controller to html

--- lib/ex338_web/controllers/fantasy_league_controller.ex
+++ lib/ex338_web/controllers/fantasy_league_controller.ex
@@ -1,5 +1,5 @@
 defmodule Ex338Web.FantasyLeagueController do
-  use Ex338Web, :controller
+  use Ex338Web, :controller_html
 
   alias Ex338.FantasyLeagues
   alias Ex338.FantasyTeams
@@ -9,7 +9,7 @@ defmodule Ex338Web.FantasyLeagueController do
 
     render(
       conn,
-      "show.html",
+      :show,
       fantasy_league: league,
       fantasy_teams: FantasyTeams.find_all_for_standings(league),
       standings_history: FantasyTeams.standings_history(league)

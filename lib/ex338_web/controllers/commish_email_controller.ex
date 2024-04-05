219809c71a76c8787907dfff4bcb8ed7f24d33ea
Axel Clark <axel.clark@pm.me>
2024-04-04 19:42:32 -0700

    Add logging for commish emails

--- lib/ex338_web/controllers/commish_email_controller.ex
+++ lib/ex338_web/controllers/commish_email_controller.ex
@@ -5,6 +5,8 @@ defmodule Ex338Web.CommishEmailController do
   alias Ex338.Repo
   alias Ex338Web.CommishNotifier
 
+  require Logger
+
   def new(conn, _params) do
     render(
       conn,
@@ -20,12 +22,16 @@ defmodule Ex338Web.CommishEmailController do
     result = CommishNotifier.send_email_to_leagues(leagues, subject, message)
 
     case result do
-      {:ok, _result} ->
+      {:ok, result} ->
+        Logger.info("Commish email sent successfully: #{inspect(result)}")
+
         conn
         |> put_flash(:info, "Email sent successfully")
         |> redirect(to: ~p"/commish_email/new")
 
       {:error, reason} ->
+        Logger.error("Commish email failed: #{inspect(reason)}")
+
         conn
         |> put_flash(:error, "Email failed to send: #{reason}")
         |> redirect(to: ~p"/commish_email/new")

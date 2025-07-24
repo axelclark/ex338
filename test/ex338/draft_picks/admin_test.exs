9e9dd2b34a49a2eb5c635d411d198a2c03f294fc
Axel Clark <axel.clark@pm.me>
2025-07-23 17:10:26 -0700

    Add automatic drafted_at update for next keeper picks
    When a player is drafted, if the next pick has a player assigned and is marked as a keeper, automatically update its drafted_at timestamp to current time.
    
    - Add maybe_update_next_keeper_drafted_at step to draft_player multi
    - Create next_pick_after_position query function in DraftPick module
    - Only updates when next pick has both player and is_keeper: true
    - Add comprehensive tests for all scenarios including integration test

--- test/ex338/draft_picks/admin_test.exs
+++ test/ex338/draft_picks/admin_test.exs
@@ -1,6 +1,8 @@
 defmodule Ex338.DraftPicks.AdminTest do
   use Ex338.DataCase, async: true
 
+  alias Ex338.DraftPicks.Admin
+
   describe "draft_player/1" do
     test "dry run draft_player ecto multi" do
       league = insert(:fantasy_league)
@@ -9,7 +11,115 @@ defmodule Ex338.DraftPicks.AdminTest do
       player = insert(:fantasy_player)
       params = %{"fantasy_player_id" => player.id}
 
-      multi = Ex338.DraftPicks.Admin.draft_player(draft_pick, params)
+      multi = Admin.draft_player(draft_pick, params)
+
+      assert [
+               {:draft_pick, {:update, _draft_pick_changeset, []}},
+               {:roster_position, {:insert, _roster_position_changeset, []}},
+               {:unavailable_draft_queues, {:update_all, _, [], returning: true}},
+               {:drafted_draft_queues, {:update_all, _, [], returning: true}}
+             ] = Ecto.Multi.to_list(multi)
+    end
+
+    test "updates next keeper pick drafted_at when next pick is a keeper with player" do
+      league = insert(:fantasy_league)
+      team1 = insert(:fantasy_team, fantasy_league: league)
+      team2 = insert(:fantasy_team, fantasy_league: league)
+
+      current_pick =
+        insert(:draft_pick,
+          fantasy_team: team1,
+          fantasy_league: league,
+          draft_position: 1.0
+        )
+
+      keeper_player = insert(:fantasy_player)
+
+      _next_pick =
+        insert(:draft_pick,
+          fantasy_team: team2,
+          fantasy_league: league,
+          draft_position: 2.0,
+          fantasy_player: keeper_player,
+          is_keeper: true,
+          drafted_at: nil
+        )
+
+      player = insert(:fantasy_player)
+      params = %{"fantasy_player_id" => player.id}
+
+      multi = Admin.draft_player(current_pick, params)
+
+      assert [
+               {:draft_pick, {:update, _draft_pick_changeset, []}},
+               {:roster_position, {:insert, _roster_position_changeset, []}},
+               {:unavailable_draft_queues, {:update_all, _, [], returning: true}},
+               {:drafted_draft_queues, {:update_all, _, [], returning: true}},
+               {:next_keeper_drafted_at, {:update, _next_pick_changeset, []}}
+             ] = Ecto.Multi.to_list(multi)
+    end
+
+    test "does not update next pick when it is not a keeper" do
+      league = insert(:fantasy_league)
+      team1 = insert(:fantasy_team, fantasy_league: league)
+      team2 = insert(:fantasy_team, fantasy_league: league)
+
+      current_pick =
+        insert(:draft_pick,
+          fantasy_team: team1,
+          fantasy_league: league,
+          draft_position: 1.0
+        )
+
+      regular_player = insert(:fantasy_player)
+
+      _next_pick =
+        insert(:draft_pick,
+          fantasy_team: team2,
+          fantasy_league: league,
+          draft_position: 2.0,
+          fantasy_player: regular_player,
+          is_keeper: false
+        )
+
+      player = insert(:fantasy_player)
+      params = %{"fantasy_player_id" => player.id}
+
+      multi = Admin.draft_player(current_pick, params)
+
+      assert [
+               {:draft_pick, {:update, _draft_pick_changeset, []}},
+               {:roster_position, {:insert, _roster_position_changeset, []}},
+               {:unavailable_draft_queues, {:update_all, _, [], returning: true}},
+               {:drafted_draft_queues, {:update_all, _, [], returning: true}}
+             ] = Ecto.Multi.to_list(multi)
+    end
+
+    test "does not update next pick when it has no player assigned" do
+      league = insert(:fantasy_league)
+      team1 = insert(:fantasy_team, fantasy_league: league)
+      team2 = insert(:fantasy_team, fantasy_league: league)
+
+      current_pick =
+        insert(:draft_pick,
+          fantasy_team: team1,
+          fantasy_league: league,
+          draft_position: 1.0
+        )
+
+      _next_pick =
+        insert(:draft_pick,
+          fantasy_team: team2,
+          fantasy_league: league,
+          draft_position: 2.0,
+          fantasy_player: nil,
+          is_keeper: true
+        )
+
+      player = insert(:fantasy_player)
+      params = %{"fantasy_player_id" => player.id}
+
+      multi = Admin.draft_player(current_pick, params)
 
       assert [
                {:draft_pick, {:update, _draft_pick_changeset, []}},
